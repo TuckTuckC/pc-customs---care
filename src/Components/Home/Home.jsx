5e1a4d230ea2ffe920776fbc118cf68120518259
Tucker Craig <tuckermcraig@gmail.com>
2024-06-30 15:24:03 -0400

    adds some catch phrases to home screen

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,17 +3,24 @@ import './Home.css';
 import ActionPic from '../../images/action-picture.png';
 
 function Home() {
-  return (
-    <div className='Home'>
-        <div className='hero'>
-            <img src={ActionPic} className='best-pc' />
-            <div>Call Now!</div>
-        </div>
-        <div>Intro / Mission Statement</div>
-        <div>2 or 3 finished PCs</div>
-        <div>Why choose us?</div>
-    </div>
-  )
+	return (
+		<div className='Home'>
+			<div className='hero'>
+				<div className='hook'>
+					<img src={ActionPic} className='best-pc' />
+					<div>
+						<h1>Choose Your Style</h1>
+						<h1>Choose Your Brand</h1>
+						<h1>Choose Your Power</h1>
+					</div>
+				</div>
+				<div>Call Now!</div>
+			</div>
+			<div>Intro / Mission Statement</div>
+			<div>2 or 3 finished PCs</div>
+			<div>Why choose us?</div>
+		</div>
+	)
 }
 
 export default Home
